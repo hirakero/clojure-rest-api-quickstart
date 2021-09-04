b5692f9b19be83a0cd7ee585fb8d4875969236a7
kero_hirake <k16.h.white@gmail.com>
2021-09-05 04:13:08 +0900

    ミニマルなAPIの実装

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[integrant "0.8.0"]
+                 [org.clojure/clojure "1.10.1"]
+                 [ring/ring-core "1.8.2"]
+                 [ring/ring-jetty-adapter "1.8.2"]]
   :main ^:skip-aot minimal-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/minimal_api/core.clj
+++ src/minimal_api/core.clj
@@ -1,7 +1,31 @@
 (ns minimal-api.core
-  (:gen-class))
+  (:gen-class)
+  (:require
+   [integrant.core :as ig]
+   [ring.adapter.jetty :as jetty]
+   [ring.util.response :as response]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+;;
+(defn hello-world [request]
+  (response/response "hello world"))
+
+(defmethod ig/init-key ::app [_ _]
+  hello-world)
+
+;;
+(defmethod ig/init-key ::server [_ {:keys [app options]}]
+  (jetty/run-jetty app options))
+
+(defmethod ig/halt-key! ::server [_ server]
+  (.stop server))
+
+;;
+(def config
+  {::app {}
+   ::server {:app (ig/ref ::app)
+             :options {:port 3000
+                       :join? false}}})
+
+;;
+(defn -main [& args]
+  (ig/init config))
