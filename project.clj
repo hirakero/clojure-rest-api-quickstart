da4eac489dd93bb758b82f2ab97675cdadf14ca7
kero_hirake <k16.h.white@gmail.com>
2021-09-06 15:32:32 +0900

    JSON変換機能追加

--- project.clj
+++ project.clj
@@ -6,7 +6,9 @@
   :dependencies [[integrant "0.8.0"]
                  [org.clojure/clojure "1.10.1"]
                  [ring/ring-core "1.8.2"]
-                 [ring/ring-jetty-adapter "1.8.2"]]
+                 [ring/ring-jetty-adapter "1.8.2"]
+                 [camel-snake-kebab "0.4.2"]
+                 [ring/ring-json "0.5.1"]]
   :main ^:skip-aot minimal-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/minimal_api/core.clj
+++ src/minimal_api/core.clj
@@ -1,31 +1,56 @@
 (ns minimal-api.core
   (:gen-class)
   (:require
+   [camel-snake-kebab.core :refer [->kebab-case ->snake_case]]
+   [camel-snake-kebab.extras :refer [transform-keys]]
    [integrant.core :as ig]
    [ring.adapter.jetty :as jetty]
+   [ring.middleware.json :refer [wrap-json-params wrap-json-response]]
+   [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+   [ring.middleware.params :refer [wrap-params]]
    [ring.util.response :as response]))
 
-;;
+;;handler
+
 (defn hello-world [request]
-  (response/response "hello world"))
+  (clojure.pprint/pprint (:params request))
+  (response/response {:message "hello world"
+                      :params (:params request)}))
+
+;;middleware
+
+(defn wrap-kebab-case-keys [handler]
+  (fn [request]
+    (let [response (-> request
+                       (update :params (partial transform-keys #(->kebab-case % :separator \_)))
+                       handler)]
+      (transform-keys #(->snake_case % :separator \-) response))))
 
 (defmethod ig/init-key ::app [_ _]
-  hello-world)
+  (-> hello-world
+      wrap-kebab-case-keys
+      wrap-keyword-params
+      wrap-json-params
+      wrap-json-response
+      wrap-params))
 
-;;
+;;server
+
 (defmethod ig/init-key ::server [_ {:keys [app options]}]
   (jetty/run-jetty app options))
 
 (defmethod ig/halt-key! ::server [_ server]
   (.stop server))
 
-;;
+;;system config
+
 (def config
   {::app {}
    ::server {:app (ig/ref ::app)
              :options {:port 3000
                        :join? false}}})
 
-;;
+;;entry point
+
 (defn -main [& args]
   (ig/init config))
