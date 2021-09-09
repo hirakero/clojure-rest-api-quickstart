91486893072d785c5a5337418cb3cebf273cd36e
kero_hirake <k16.h.white@gmail.com>
2021-09-09 12:07:01 +0900

    replからのテスト実行を追加

--- dev/src/dev.clj
+++ dev/src/dev.clj
@@ -1,8 +1,10 @@
 (ns dev
+  (:refer-clojure :exclude [test])
   (:require
    [clojure.java.io :as io]
    [clojure.repl :refer :all]
    [clojure.spec.alpha :as s]
+   [clojure.test :as test]
    [clojure.tools.namespace.repl :refer [refresh]]
    [integrant.core :as ig]
    [integrant.repl :refer [clear halt go init prep reset]]
@@ -10,3 +12,7 @@
    [minimal-api.core]))
 
 (integrant.repl/set-prep! (constantly minimal-api.core/config))
+
+(defn test
+  ([] (test/run-all-tests #"minimal-api-lein\..+-test"))
+  ([ns-sym] (test/run-tests ns-sym)))
